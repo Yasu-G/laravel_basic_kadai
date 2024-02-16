a2305e45190818455b7aa9d374dc26de612b7e84
Yasu-G <140491519+Yasu-G@users.noreply.github.com>
2024-02-16 14:28:44 +0900

    Laravel_basic_kadai_0216

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,19 @@
   <title>Laravel-basic-kadai</title>
 </head>
 <body>
-  <h1>投稿一覧</h1>
+  <table>
+    <tr>
+        <th>タイトル</th>
+        <th>本文</th>
+    </tr>
+
+    @foreach ($posts as $post)
+        <tr>
+            <td> {{ $post->title }} </td>
+            <td> {{ $post->content }} </td>
+        </tr>
+    @endforeach
+
+  </table>
 </body>
 </html>
